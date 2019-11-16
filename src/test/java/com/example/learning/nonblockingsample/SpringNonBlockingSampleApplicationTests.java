c7c02b69bb0b583d45d96286c6b859a0b5d17c2c
Masahiko Kaiho <mkaiho.cgengo@gmail.com>
2019-11-16 22:53:59 +0900

    Generated source codes by tool.

---
+++ src/test/java/com/example/learning/nonblockingsample/SpringNonBlockingSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learning.nonblockingsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNonBlockingSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
